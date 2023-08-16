e1a63ee5d2ca30db9b69eebf745b8f25e229b5d6
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-15 23:37:13 -0400

    Fixed Linux support

--- Library/Sources/CampingService/Extensions/Locale.swift
+++ Library/Sources/CampingService/Extensions/Locale.swift
@@ -10,10 +10,14 @@ import Foundation
 internal extension Locale {
     
     var _languageCode: String {
+        #if canImport(Darwin)
         if #available(macOS 13, iOS 16, tvOS 16, watchOS 9, *) {
             return language.minimalIdentifier
         } else {
             return languageCode ?? "en"
         }
+        #else
+        return languageCode ?? "en"
+        #endif
     }
 }
