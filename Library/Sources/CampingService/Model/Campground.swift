3a05736a73cee89325fe0761cdbe41555f35e9c8
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-16 00:18:49 -0400

    Add `Campground`

---
+++ Library/Sources/CampingService/Model/Campground.swift
@@ -0,0 +1,49 @@
+//
+//  Campground.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/15/23.
+//
+
+import Foundation
+
+/// Campground Location
+public struct Campground: Equatable, Hashable, Codable, Identifiable {
+    
+    public let id: UUID
+    
+    public var name: String
+    
+    public var address: String
+    
+    public var location: LocationCoordinates
+    
+    public var amenities: [Amenity]
+    
+    public var phoneNumber: String?
+    
+    public var descriptionText: String
+    
+    public var notes: String?
+    
+    public init(
+        id: UUID = UUID(),
+        name: String,
+        address: String,
+        location: LocationCoordinates,
+        amenities: [Amenity] = [],
+        phoneNumber: String? = nil,
+        descriptionText: String,
+        notes: String? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.address = address
+        self.location = location
+        self.amenities = amenities
+        self.phoneNumber = phoneNumber
+        self.descriptionText = descriptionText
+        self.notes = notes
+    }
+}
+
