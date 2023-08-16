b91dd66ba8e94b80a89ebec62e25047b42f264e2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-15 21:58:57 -0400

    Add `User`

---
+++ LIbrary/Sources/CampingService/User.swift
@@ -0,0 +1,53 @@
+//
+//  User.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/15/23.
+//
+
+import Foundation
+
+/// User
+public struct User: Equatable, Hashable, Codable, Identifiable {
+            
+    public let id: UUID
+    
+    public let created: Date
+    
+    public var email: String
+    
+    public var role: Role
+    
+    public var firstName: String
+    
+    public var lastName: String
+    
+    public var phoneNumber: String? = nil
+    
+    public init(
+        id: UUID = UUID(),
+        email: String,
+        firstName: String,
+        lastName: String,
+        role: Role = .client,
+        phoneNumber: String? = nil
+    ) {
+        self.id = id
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        self.role = role
+        self.phoneNumber = phoneNumber
+        self.created = Date()
+    }
+}
+
+public extension User {
+    
+    enum Role: Int, Codable, CaseIterable {
+        
+        case client = 0
+        
+        case admin = 999
+    }
+}
