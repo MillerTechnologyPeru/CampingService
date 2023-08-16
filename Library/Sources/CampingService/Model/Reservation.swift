578b398dedbbdacecfc4e1247e74fdc2a5780729
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-16 02:17:27 -0400

    Add `Reservation.init()`

--- Library/Sources/CampingService/Model/Reservation.swift
+++ Library/Sources/CampingService/Model/Reservation.swift
@@ -23,6 +23,24 @@ public struct Reservation: Equatable, Hashable, Codable, Identifiable {
     public let unit: RentalUnit.ID
     
     public var status: Status
+    
+    public init(
+        id: UUID = UUID(),
+        created: Date = Date(),
+        start: Date,
+        end: Date,
+        user: User.ID,
+        unit: RentalUnit.ID,
+        status: Reservation.Status = .reserved
+    ) {
+        self.id = id
+        self.created = created
+        self.start = start
+        self.end = end
+        self.user = user
+        self.unit = unit
+        self.status = status
+    }
 }
 
 public extension Reservation {
