c8cc4f50f88447558a4be3b381dd8c568b6f9ca8
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-15 21:35:31 -0400

    Created Xcode project

---
+++ Xcode/CampingApp/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  CampingApp
+//
+//  Created by Alsey Coleman Miller on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CampingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode/CampingKit/CampingKit.swift
@@ -0,0 +1,11 @@
+//
+//  CampingKit.swift
+//  CampingKit
+//
+//  Created by Alsey Coleman Miller on 8/15/23.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+@_exported import CampingService
