9c3f7612ca3ca2ff35377256c741e58126aa0ac6
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-16 00:18:19 -0400

    Add `Amenity`

---
+++ Library/Sources/CampingService/Model/Amenity.swift
@@ -0,0 +1,32 @@
+//
+//  Amenities.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/16/23.
+//
+
+import Foundation
+
+/// Campground Amenities
+public enum Amenity: String, Codable, CaseIterable {
+    
+    case water
+    case amp30
+    case amp50
+    case wifi
+    case laundry
+    case mail
+    case dumpStation
+    case picnicArea
+    case storage
+    case cabins
+    case showers
+    case restrooms
+    case fishing
+    case beach
+    case lake
+    case river
+    case rv
+    case tent
+    case pets
+}
