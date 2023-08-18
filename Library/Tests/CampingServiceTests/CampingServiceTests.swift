52f59a1a00a4ac79250f9d71959a53f488e78864
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-18 13:43:29 -0400

    Updated unit tests

--- Library/Tests/CampingServiceTests/CampingServiceTests.swift
+++ Library/Tests/CampingServiceTests/CampingServiceTests.swift
@@ -1,12 +1,40 @@
+import Foundation
 import XCTest
+#if canImport(CoreLocation)
+import CoreLocation
+#endif
+import CoreModel
 @testable import CampingService
 
 final class CampingServiceTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documenation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testAmenities() throws {
+        
+        let amenitites = Amenity.allCases
+        let expectedValue = AttributeValue.string("[\"water\",\"amp30\",\"amp50\",\"wifi\",\"laundry\",\"mail\",\"dumpStation\",\"picnicArea\",\"storage\",\"cabins\",\"showers\",\"restrooms\",\"pool\",\"fishing\",\"beach\",\"lake\",\"river\",\"rv\",\"tent\",\"pets\"]")
+        XCTAssertEqual(amenitites.attributeValue, expectedValue)
+        XCTAssertEqual([Amenity](attributeValue: expectedValue), amenitites)
+    }
+    
+    func testLocation() {
+        
+        let location = LocationCoordinates(
+            latitude: -59.709701,
+            longitude: 157.668416
+        )
+        
+        let expectedValue = AttributeValue.string("-59.709701,157.668416")
+        XCTAssertEqual(location.attributeValue, expectedValue)
+        XCTAssertEqual(LocationCoordinates(attributeValue: expectedValue), location)
+        
+        #if canImport(CoreLocation)
+        let coreLocationValue = CLLocationCoordinate2D(
+            latitude: -59.709701,
+            longitude: 157.668416
+        )
+        XCTAssertEqual(LocationCoordinates(coordinates: coreLocationValue), location)
+        XCTAssertEqual(CLLocationCoordinate2D(location: location).latitude, coreLocationValue.latitude)
+        XCTAssertEqual(CLLocationCoordinate2D(location: location).longitude, coreLocationValue.longitude)
+        #endif
     }
 }
