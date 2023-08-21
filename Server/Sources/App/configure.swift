55620482eecb9bd5c957137b72ce9455cab2ed32
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-20 22:41:40 -0400

    Updated `configure()`

--- Server/Sources/App/configure.swift
+++ Server/Sources/App/configure.swift
@@ -5,33 +5,35 @@ import MongoDBModel
 import CampingService
 
 // configures your application
-public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    /*
-    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-     */
+extension CampingServer {
     
-    let connection = "mongodb://localhost:27017"
-    let mongoClient = try MongoClient(connection, using: app.eventLoopGroup)
-    let databaseName = Environment.get("DATABASE_NAME") ?? "camping"
-    let database = MongoModelStorage(
-        database: mongoClient.db(databaseName),
-        model: .camping
-    )
-    
-    // configure JSON encoder
-    ContentConfiguration.global.use(encoder: JSONEncoder.camping, for: .json)
-    ContentConfiguration.global.use(decoder: JSONDecoder.camping, for: .json)
-    
-    // register routes
-    try routes(app, database: database)
+    static func configure(_ app: Application) async throws {
+        
+        // configure MongoDB connection
+        let hostname = Environment.get("DATABASE_HOST") ?? "localhost"
+        let port = Environment.get("DATABASE_PORT").flatMap(UInt.init(_:)) ?? 27017
+        let databaseName = Environment.get("DATABASE_NAME") ?? "camping"
+        let connection = "mongodb://\(hostname):\(port)"
+        var options = MongoClientOptions()
+        if let username = Environment.get("DATABASE_USERNAME"),
+           let password = Environment.get("DATABASE_PASSWORD") {
+            options.credential = .init(
+                username: username,
+                password: password
+            )
+        }
+        let mongoClient = try MongoClient(connection, using: app.eventLoopGroup, options: options)
+        self.mongoClient = mongoClient
+        let database = MongoModelStorage(
+            database: mongoClient.db(databaseName),
+            model: .camping
+        )
+        
+        // configure JSON encoder
+        ContentConfiguration.global.use(encoder: JSONEncoder.camping, for: .json)
+        ContentConfiguration.global.use(decoder: JSONDecoder.camping, for: .json)
+        
+        // register routes
+        try routes(app, database: database)
+    }
 }
