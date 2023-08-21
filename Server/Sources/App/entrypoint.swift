554b7f7a7d04490fc71fa07e84d782463108de16
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-20 22:36:12 -0400

    Fix MongoSwift cleanup

--- Server/Sources/App/entrypoint.swift
+++ Server/Sources/App/entrypoint.swift
@@ -1,6 +1,36 @@
-import Vapor
+import Foundation
 import Dispatch
 import Logging
+import Vapor
+import MongoSwift
+
+@main
+enum CampingServer {
+    
+    static var mongoClient: MongoClient?
+    
+    static func main() async throws {
+        var env = try Environment.detect()
+        try LoggingSystem.bootstrap(from: &env)
+        
+        let app = Application(env)
+        defer {
+            app.shutdown()
+            if let client = mongoClient {
+                try? client.syncClose()
+                cleanupMongoSwift()
+            }
+        }
+        
+        do {
+            try await configure(app)
+        } catch {
+            app.logger.report(error: error)
+            throw error
+        }
+        try await app.runFromAsyncMainEntrypoint()
+    }
+}
 
 /// This extension is temporary and can be removed once Vapor gets this support.
 private extension Vapor.Application {
@@ -17,24 +47,5 @@ private extension Vapor.Application {
                 }
             }
         }
-    }
-}
-
-@main
-enum Entrypoint {
-    static func main() async throws {
-        var env = try Environment.detect()
-        try LoggingSystem.bootstrap(from: &env)
-        
-        let app = Application(env)
-        defer { app.shutdown() }
-        
-        do {
-            try await configure(app)
-        } catch {
-            app.logger.report(error: error)
-            throw error
-        }
-        try await app.runFromAsyncMainEntrypoint()
     }
 }
