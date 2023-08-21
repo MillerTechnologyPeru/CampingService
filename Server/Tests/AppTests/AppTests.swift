36128b50daad9a3780f1df2abaf9c16fdd2147b3
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-20 22:41:46 -0400

    Updated unit tests

--- Server/Tests/AppTests/AppTests.swift
+++ Server/Tests/AppTests/AppTests.swift
@@ -6,11 +6,13 @@ final class AppTests: XCTestCase {
     func testHelloWorld() async throws {
         let app = Application(.testing)
         defer { app.shutdown() }
-        try await configure(app)
-
+        try await CampingServer.configure(app)
+        
+        #if DEBUG
         try app.test(.GET, "hello", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
+        #endif
     }
 }
