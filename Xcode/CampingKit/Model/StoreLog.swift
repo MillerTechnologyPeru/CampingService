e26fdb77495e631f06db8045c6cafb1913e40ce3
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-25 03:12:41 -0400

    Updated error logging

--- Xcode/CampingKit/Model/StoreLog.swift
+++ Xcode/CampingKit/Model/StoreLog.swift
@@ -18,10 +18,23 @@ public extension Store {
         let subsystem = Bundle.main.bundleIdentifier ?? "com.colemancda.CampingKit"
         let logger = Logger(subsystem: subsystem, category: category.rawValue)
         logger.log(level: level, "\(message)")
+        // TODO: Log to file
+        
     }
     
-    func logError(_ error: Error, category: LogCategory = .app) {
-        log("⚠️ " + error.localizedDescription, level: .error, category: category)
+    func logError(
+        _ error: Error,
+        category: LogCategory = .app,
+        file: StaticString = #file,
+        function: StaticString = #function
+    ) {
+        let message: String
+        #if DEBUG
+        message = "⚠️ " + error.localizedDescription + String(file) + String(function)
+        #else
+        message = "⚠️ " + error.localizedDescription
+        #endif
+        log(message, level: .error, category: category)
     }
 }
 
