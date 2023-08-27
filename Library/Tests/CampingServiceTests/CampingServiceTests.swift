2671f1a21712835d67a1e73bc1c850aa39f34ade
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-26 20:52:00 -0400

    Updated unit tests

--- Library/Tests/CampingServiceTests/CampingServiceTests.swift
+++ Library/Tests/CampingServiceTests/CampingServiceTests.swift
@@ -8,6 +8,22 @@ import CoreModel
 
 final class CampingServiceTests: XCTestCase {
     
+    func testURL() {
+        
+        let values: [(CampingURL, String)] = [
+            (.campground(Campground.ID(uuidString: "30D9213F-2C33-4C2E-839C-77038AFBD60C")!), "x-camping-service:/campground/30D9213F-2C33-4C2E-839C-77038AFBD60C"),
+            (.location(Campground.ID(uuidString: "15EF5BF2-A07A-476C-BA1B-7A7A4127C806")!), "x-camping-service:/location/15EF5BF2-A07A-476C-BA1B-7A7A4127C806"),
+            (.reservation(Campground.ID(uuidString: "2A0EA762-D0C2-43C7-9422-62AE464A8360")!), "x-camping-service:/reservation/2A0EA762-D0C2-43C7-9422-62AE464A8360")
+        ]
+        
+        for (url, string) in values {
+            XCTAssertEqual(CampingURL(rawValue: string), url)
+            XCTAssertEqual(url.rawValue, string)
+            XCTAssertEqual(url.description, string)
+            XCTAssertEqual(URL(url).absoluteString, string)
+        }
+    }
+    
     func testAmenities() throws {
         
         let amenitites = Amenity.allCases
